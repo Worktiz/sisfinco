deb750e58ea659569c09e7eb65e38c831bb28c19
Wilka Ortiz <wdejom@gmail.com>
2023-03-10 08:13:41 -0400

    ADD FILES

---
+++ sistema/client.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Sisfinco</title>
+	<link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+	<?php include "includes/header.php"; ?>
+
+    <a href="new_client.php"></a>
+
+
+</body>
+</html>
