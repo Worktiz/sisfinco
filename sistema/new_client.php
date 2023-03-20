565f27283d550c41cab0488d5f79241e627fef5a
Wilka Ortiz <wdejom@gmail.com>
2023-03-20 16:14:45 -0400

    REPAIR

--- sistema/new_client.php
+++ sistema/new_client.php
@@ -4,11 +4,11 @@
 
 	if (!empty($_POST)) {
 		$alert= '';
-		if (empty($_POST['cedularif']) || empty($_POST['nombre']) || empty($_POST['correo']) || empty($_POST['telefono']) || empty($_POST['direccion'])) {
+		if (empty($_POST['cedula']) || empty($_POST['nombre']) || empty($_POST['correo']) || empty($_POST['telefono']) || empty($_POST['direccion'])) {
 			$alert= '<p class="msg_error">Todos Los Campos Son Obligatorios.</p>';
 		}else{
 			
-            $cedularif  = $_POST['cedularif'];
+            $cedula  = $_POST['cedula'];
 			$nombre     = $_POST['nombre'];
 			$email      = $_POST['correo'];
 			$telefono   = $_POST['telefono'];
@@ -18,16 +18,16 @@
             $result = 0;
 
 
-            if (is_numeric($cedularif)) {
-                $query = mysqli_query($conection,"SELECT * FROM cliente WHERE cedularif = '$cedularif'");
+            if (is_numeric($cedula)) {
+                $query = mysqli_query($conection,"SELECT * FROM cliente WHERE cedula = '$cedula'");
                 $result = mysqli_fetch_array($query);
             }
 
             if ($result > 0) {
                 $alert = '<p class="msg_error">El Numero de Cedula o RIF Ya existe</p>';
             }else{
-                $query_insert = mysqli_query($conection,"INSERT INTO cliente(cedularif,nombre,correo,telefono,direccion,id_usuario) 
-                                                            VALUES('$cedularif',  '$nombre', '$email', '$telefono',  '$direccion', '$id_usuario')");
+                $query_insert = mysqli_query($conection,"INSERT INTO cliente(cedula,nombre,correo,telefono,direccion,id_usuario) 
+                                                            VALUES('$cedula',  '$nombre', '$email', '$telefono',  '$direccion', '$id_usuario')");
 
                     if ($query_insert) {
                         $alert = '<p class="msg_save">Cliente Guardado Correctamente</p>';
@@ -56,8 +56,8 @@
 		<h4>Registro de Cliente</h4>
 		<div class="alert"><?php echo isset($alert) ? $alert : ''; ?></div>
 		<form action="" method="POST">
-        <label for="cedularif">Ingrese Cedula</label>
-		<input class="controls" type="number" name="cedularif" id="cedularif" placeholder="Ingrese Cedula">
+        <label for="cedula">Ingrese Cedula</label>
+		<input class="controls" type="number" name="cedula" id="cedula" placeholder="Ingrese Cedula">
 		<label for="nombre">Nombre y Apellido</label>
 		<input class="controls" type="text" name="nombre" id="nombre" placeholder="Ingrese Nombre y Apellido">
 		<label for="correo">Correo</label>
